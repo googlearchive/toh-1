cec1eb2e181142cad90d1824251ab95437ff9677
Patrice Chalin <pchalin@gmail.com>
2018-02-21 12:52:41 -0500

    Sync with 68524eb
    Synced with dart-lang/site-webdev 5-dev branch, commit 68524eb:
    https://github.com/dart-lang/site-webdev/tree/68524eb3e91364762c9d7510f95c75673a183d44/examples/ng/doc/toh-1

--- 5/packages/angular_tour_of_heroes/app_component.template.dart
+++ 5/packages/angular_tour_of_heroes/app_component.template.dart
@@ -9,11 +9,9 @@ import 'app_component.dart';
 export 'app_component.dart';
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
-// Required for initReflector().
 import 'package:angular/src/di/reflector.dart' as _ngRef;
 import 'package:angular/angular.template.dart' as _ref0;
 import 'package:angular_forms/angular_forms.template.dart' as _ref1;
-
 import 'package:angular/src/core/linker/app_view.dart';
 import 'app_component.dart' as import1;
 import 'dart:html' as import2;
@@ -186,10 +184,8 @@ void initReflector() {
     return;
   }
   _visited = true;
+
+  _ngRef.registerComponent(AppComponent, AppComponentNgFactory);
   _ref0.initReflector();
   _ref1.initReflector();
-  _ngRef.registerComponent(
-    AppComponent,
-    AppComponentNgFactory,
-  );
 }
